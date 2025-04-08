37c6aa32f4b5eb7adac931f7ad6565f8126e2542
Akash-b586 <153656980+Akash-b586@users.noreply.github.com>
2025-04-08 19:07:00 +0530

    minor fixes

--- frontend/src/pages/AdminRegister.tsx
+++ frontend/src/pages/AdminRegister.tsx
@@ -20,7 +20,7 @@ import MuiCard from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
 import AppTheme from '../shared-theme/AppTheme';
 import ColorModeSelect from '../shared-theme/ColorModeSelect';
-import { GoogleIcon, FacebookIcon } from '../components/CustomIcons';
+import { GoogleIcon, FacebookIcon } from '../components/auth/CustomIcons';
 import Cookies from 'js-cookie';
 
 const Card = styled(MuiCard)(({ theme }) => ({
