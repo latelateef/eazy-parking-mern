6a025f6214e92bbd0a9b4a5514a3bf24045771ab
Shubham Saini <sainishubham9928@gmail.com>
2025-04-14 00:40:28 +0530

    added loader

--- frontend/src/components/auth/GoogleButton.tsx
+++ frontend/src/components/auth/GoogleButton.tsx
@@ -1,15 +1,20 @@
+import { ThemeContext } from "@/context/ThemeContext";
 import { BACKEND_URL } from "@/utils/backend";
 import { GoogleLogin } from "@react-oauth/google";
+import { Spin } from "antd";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { useContext, useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 const GoogleButton = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const responseMessage = async (response: any) => {
     const token = response.credential;
     try {
+      setLoading(true);
       const res = await axios.post(`${BACKEND_URL}/api/user/googleAuth`, {
         token,
       });
@@ -25,18 +30,26 @@ const GoogleButton = () => {
       }
     } catch (error) {
       console.error("Error during Google login:", error);
+    } finally {
+      setLoading(false);
     }
   };
   const errorMessage = () => {
     console.log("An error occurred during Google login.");
   };
+  const { theme } = useContext(ThemeContext);
   return (
     <div>
-      <GoogleLogin
-        onSuccess={responseMessage}
-        onError={errorMessage}
-        useOneTap={false}
-      />
+      {loading && <Spin fullscreen size="large"
+      tip={<div className="text-lg">Please Wait!</div>} spinning/>}
+      <center>
+        <GoogleLogin
+          onSuccess={responseMessage}
+          onError={errorMessage}
+          useOneTap={false}
+          theme={theme == "light" ? "outline" : "filled_black"}
+        />
+      </center>
     </div>
   );
 };
